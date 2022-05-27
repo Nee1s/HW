15424a703abd43f169e74a5a6cd38a1ef8377d8e
Nee1s <kassiasj@yandex.ru>
2022-05-27 13:57:01 +0400

    dz4-5 done

--- lib/constants/queries.dart
+++ lib/constants/queries.dart
@@ -3,6 +3,7 @@ const String hostUrl = 'yummly2.p.rapidapi.com';
 
 const String prefixApiKey = 'X-RapidAPI-Key';
 const String apiKey = '2c21aeb330mshe372d92111fe6a9p12086bjsnb7624140804a';
+const String tagsPrefix = 'allowedAttribute';
 
 const String linkAllRecipes = 'https://yummly2.p.rapidapi.com/feeds/list';
 const String linkSearchRecipes = 'https://yummly2.p.rapidapi.com/feeds/search';
